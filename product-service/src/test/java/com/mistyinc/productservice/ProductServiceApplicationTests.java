c31ade4b04ed868a34d3f917d8cbcf312a13ff89
moseswuniche <moses@formpl.us>
2023-07-28 18:32:13 +0000

    Initial commit, Added first service, product-service.

---
+++ product-service/src/test/java/com/mistyinc/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mistyinc.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
